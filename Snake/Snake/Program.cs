7233252f151ac1495840d5454327910b94aa52d9
LirraSmile <lirrasmile@gmail.com>
2016-04-09 14:31:44 +0700

    Vertical lines and figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,28 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+ 
+            // Draw frame
+            HorizontalLine lineUp = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine lineDown = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine lineLeft = new VerticalLine(0, 24, 0, '^');
+            VerticalLine lineRight = new VerticalLine(0, 24, 78, '^');
+
+            lineUp.Drow();
+            lineDown.Drow();
+            lineLeft.Drow();
+            lineRight.Drow();
+
+            // Draw points
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(2, 4, '#');
             p2.Draw();
+
+
 
 
             Console.ReadLine();
